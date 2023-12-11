0f3468779046b70c57fc8fb31a9db6481d5014d9
Yerke Nurgali <70740293+qelephant@users.noreply.github.com>
2023-12-11 16:51:18 +0600

    Update InterfaceCreator.java

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -3,6 +3,8 @@ package com.epam.mjc;
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return list -> list.stream()
+                .map(value -> value / divider)
+                .collect(Collectors.toList());
     }
 }
